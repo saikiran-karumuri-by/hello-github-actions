3da5c5a9d87163d8b2f794f6d4bc803fd36977bc
saikiran-karumuri-by <saikiran.karumuri@blueyonder.com>
2020-11-02 21:46:07 -0700

    test merge

--- action-a/git-message.sh
+++ action-a/git-message.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 Git_Repo=$1
 Git_Token=$2
+Git_Current_Branch=$3
+Git_Target_Branch=$4
 
 echo "https://api.github.com/repos/$Git_Repo/merges"
-          jq -nc '{"base": "master","head":"testpr","commit_message":"My First PR merge"}' | \
+          jq -nc '{"base": "'"Git_Target_Branch"'","head":"'"Git_Current_Branch"'","commit_message":"My First PR merge"}' | \
           curl -sL  -X POST -d @- \
             -H "Content-Type: application/json" \
             -H "Authorization: token $Git_Token" \
